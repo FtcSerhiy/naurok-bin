6de46ec551d87886f99e518d36e0eff9837248a6
Ftc Serhiy <ftcserhiy@gmail.com>
2022-11-26 14:04:02 +0200

    test version

--- script.js
+++ script.js
@@ -6,11 +6,33 @@ document.querySelector('button').addEventListener('click', e => {
     const amountQuestions = document.querySelector('#amount').value
     const subject = document.querySelector('#subject').value
     const klass = document.querySelector('#klas').value
-	let response;
-
-	fetch(`${url}?name=${testName}&amount=${amountQuestions}&subject=${subject}&klass=${klass}`)
-		.then(request => response = request)
-
-	console.log(response)
-	document.querySelector('main').innertHTML = response
+	const data = {
+		name: testName,
+		amount: amountQuestions,
+		subject: subject,
+		klass: klass
+	}
+	getResponse(url, data)
 })
+
+function displayResponseData(data) {
+	const main = document.querySelector('main')
+	document.querySelector('form').remove()
+	const tag = document.querySelector('main')
+
+	for (let name in data) {
+		let div = document.createElement('div')
+		div.innerHTML = `<div class="name">${name}</div> <div class="response">${data[name]}</div>`
+		tag.appendChild(div)
+	}
+}
+
+function getResponse(url, data) {
+	fetch(url, {
+		method: 'POST',
+		headers: {'Content-Type': 'application/json'}, 
+  		body: JSON.stringify(data)
+	})
+		.then(request => request.json().then(response => displayResponseData(response.value)).catch(err => console.log(err)))
+		.catch(err => console.log(err))
+}
