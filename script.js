81daff1045ef1c2c4c56f8b10b98d6df34e104a2
Ftc Serhiy <ftcserhiy@gmail.com>
2022-11-29 11:39:36 +0200

    add search

--- script.js
+++ script.js
@@ -12,18 +12,41 @@ document.querySelector('button').addEventListener('click', e => {
 		subject: subject,
 		klass: klass
 	}
-	getResponse(url, data)
+	document.querySelector('form').remove()
+	document.querySelector('main').innerHTML = '<span style="color:grey">Click \'/\' for open search</span>'
+	
+	displayResponseData(data)
+	document.addEventListener('keydown', e => {
+		if (e.code === 'Slash') {
+			const popup = document.querySelector('.popup')
+			popup.classList.add('open')
+			document.addEventListener('keydown', e => {
+				if(e.code === 'Escape') {document.querySelector('.popup').classList.remove('open')}
+			})
+			
+			document.querySelector('.popup__content').innerHTML = `
+			<form class="find">
+				<input type="text" placehodler="Напишіть запитання...">
+				<button>/</button>
+			</form>
+			<hr>`
+
+			document.querySelector('button').addEventListener('click', e => {
+				e.preventDefault()
+				const key = document.querySelector('input').value
+
+				displayRequest(filterRequests(Object.keys(data), key), data)
+			})
+		}
+	})
 })
 
 function displayResponseData(data) {
-	const main = document.querySelector('main')
-	document.querySelector('form').remove()
-	const tag = document.querySelector('main')
-
 	for (let name in data) {
 		let div = document.createElement('div')
+		div.classList.add('block')
 		div.innerHTML = `<div class="name">${name}</div> <div class="response">${data[name]}</div>`
-		tag.appendChild(div)
+		document.querySelector('main').appendChild(div)
 	}
 }
 
@@ -36,3 +59,24 @@ function getResponse(url, data) {
 		.then(request => request.json().then(response => displayResponseData(response.value)).catch(err => console.log(err)))
 		.catch(err => console.log(err))
 }
+
+function filterRequests(arr, searchKey) {
+	const request = arr.filter(e => e.includes(searchKey))
+	console.log(request)
+    return request
+}
+
+function displayRequest(request, data) {
+	const block = document.querySelector('.block')
+	if(block) {
+		block.remove()
+		console.log(block)
+	}
+	const element = document.createElement('div')
+	element.classList.add('block')
+	request.forEach(e => {
+		element.innerHTML = `<div class="name">${e}</div> <div class="response">${data[e]}</div>`
+	})
+	document.querySelector('.popup__content').appendChild(element)
+}
+
